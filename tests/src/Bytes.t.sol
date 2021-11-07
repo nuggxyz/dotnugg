c5b41c7c448900965e2ae04ad39d3a49ffd63ff3
dub6ix <dub6ix@nugglabs.org>
2021-11-07 11:13:59 -0600

    toUint4 test

---
+++ tests/src/Bytes.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import '../lib/DSTest.sol';
+import '../../contracts/libraries/Bytes.sol';
+
+contract BytesTest is DSTest {
+    function test_toUint4() public {
+        bytes memory _bytes = hex'2c';
+        uint256 _start = 0;
+
+        (uint8 res0, uint8 res1) = Bytes.toUint4(_bytes, _start);
+
+        emit log_named_uint('res0', res0);
+        emit log_named_uint('res1', res1);
+
+        assertEq(res0, 2);
+        assertEq(res1, 12);
+    }
+}
