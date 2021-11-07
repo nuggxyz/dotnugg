4bef4704085c769542e4fef3450d6f90424803a4
dub6ix <dub6ix@nugglabs.org>
2021-11-07 13:59:50 -0600

    eq matrix set test

--- tests/src/MatrixHelper.t.sol
+++ tests/src/MatrixHelper.t.sol
@@ -26,6 +26,7 @@ contract MatrixHelperTest is DSTest {
         IDotNugg.Pixel[] memory pallet = new IDotNugg.Pixel[](2);
         pallet[0] = IDotNugg.Pixel({rgba: IDotNugg.Rgba({r: 1, g: 1, b: 1, a: 255}), zindex: 2, exists: true});
         pallet[1] = IDotNugg.Pixel({rgba: IDotNugg.Rgba({r: 255, g: 255, b: 255, a: 27}), zindex: 3, exists: true});
+
         IDotNugg.Matrix memory matrix = Matrix.create(width, height);
 
         Matrix.set(matrix, data, pallet, groupWidth);
@@ -43,5 +44,46 @@ contract MatrixHelperTest is DSTest {
         }
 
         assertEq(count, 18);
+    }
+
+    function test_matrix_set_1() public {
+        // arguments
+        uint8 width = 33;
+        uint8 height = 33;
+        bytes memory data = hex'051105110511';
+        uint8 groupWidth = 9;
+        IDotNugg.Pixel[] memory pallet = new IDotNugg.Pixel[](2);
+        pallet[0] = IDotNugg.Pixel({rgba: IDotNugg.Rgba({r: 1, g: 1, b: 1, a: 255}), zindex: 2, exists: true});
+        pallet[1] = IDotNugg.Pixel({rgba: IDotNugg.Rgba({r: 255, g: 255, b: 255, a: 27}), zindex: 3, exists: true});
+
+        IDotNugg.Matrix memory want = Matrix.create(width, height);
+
+        want.data[0][0] = pallet[0];
+        want.data[0][1] = pallet[0];
+        want.data[0][2] = pallet[0];
+        want.data[0][3] = pallet[0];
+        want.data[0][4] = pallet[0];
+        want.data[0][5] = pallet[1];
+        want.data[0][6] = pallet[0];
+        want.data[0][7] = pallet[0];
+        want.data[0][8] = pallet[0];
+        want.data[1][0] = pallet[0];
+        want.data[1][1] = pallet[0];
+        want.data[1][2] = pallet[1];
+        want.data[1][3] = pallet[0];
+        want.data[1][4] = pallet[0];
+        want.data[1][5] = pallet[0];
+        want.data[1][6] = pallet[0];
+        want.data[1][7] = pallet[0];
+        want.data[1][8] = pallet[1];
+
+        want.width = 9;
+        want.height = 2;
+
+        IDotNugg.Matrix memory got = Matrix.create(width, height);
+
+        Matrix.set(got, data, pallet, groupWidth);
+
+        assertEq(got, want);
     }
 }
