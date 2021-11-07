75ccf8de8c1c8575ccd2f850d8d3145d2d1066e3
danny7even <danny7even@nugglabs.org>
2021-11-07 17:17:28 +0100

    cool

--- tests/src/MatrixHelper.t.sol
+++ tests/src/MatrixHelper.t.sol
@@ -3,20 +3,15 @@ pragma solidity ^0.8.4;
 
 import "../lib/DSTest.sol";
 
-import "./MatrixHelper.sol";
 import '../../contracts/interfaces/IDotNugg.sol';
+import '../../contracts/logic/Matrix.sol';
 
 contract MatrixHelperTest is DSTest {
-    MatrixHelper matrixHelper;
 
-    function setUp() public {
-        matrixHelper = new MatrixHelper();
-    }
-
-    function test_create() public {
+    function test_matrix_create() public {
         uint8 width = 33;
         uint8 height = 33;
-        IDotNugg.Matrix memory matrix = matrixHelper.create(width, height);
+        IDotNugg.Matrix memory matrix = Matrix.create(width, height);
 
         assertEq(width, matrix.data[0].length);
         assertEq(height, matrix.data.length);
