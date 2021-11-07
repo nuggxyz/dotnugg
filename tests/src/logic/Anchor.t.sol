0b07f04d24279786383423bd40465e36f293f83b
danny7even <danny7even@nugglabs.org>
2021-11-07 21:20:36 +0100

    starting tests

---
+++ tests/src/logic/Anchor.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import '../../lib/DSTest.sol';
+import '../../../contracts/interfaces/IDotNugg.sol';
+import '../../../contracts/logic/Matrix.sol';
+
+
+contract AnchorTest is DSTest {
+    using Matrix for IDotNugg.Matrix;
+
+    function test_getAnchors() public {
+        
+    }
+}
