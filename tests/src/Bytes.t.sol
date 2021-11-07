248e3305a46e7e687f103887c9ee07d620286b0c
dub6ix <dub6ix@nugglabs.org>
2021-11-07 11:29:53 -0600

    testing two's complements

--- tests/src/Bytes.t.sol
+++ tests/src/Bytes.t.sol
@@ -17,4 +17,59 @@ contract BytesTest is DSTest {
         assertEq(res0, 2);
         assertEq(res1, 12);
     }
+
+    function test_toInt8_0() public {
+        bytes memory _bytes = hex'2c';
+        uint256 _start = 0;
+
+        int8 res0 = Bytes.toInt8(_bytes, _start);
+
+        emit log_named_int('res0', res0);
+
+        assertEq(res0, 44);
+    }
+
+    function test_toInt8_1() public {
+        bytes memory _bytes = hex'f4';
+        uint256 _start = 0;
+
+        int8 res0 = Bytes.toInt8(_bytes, _start);
+
+        emit log_named_int('res0', res0);
+
+        assertEq(res0, -12);
+    }
+
+    function test_toInt8_2() public {
+        bytes memory _bytes = hex'cd';
+        uint256 _start = 0;
+
+        int8 res0 = Bytes.toInt8(_bytes, _start);
+
+        emit log_named_int('res0', res0);
+
+        assertEq(res0, -51);
+    }
+
+    function test_toInt8_3() public {
+        bytes memory _bytes = hex'00';
+        uint256 _start = 0;
+
+        int8 res0 = Bytes.toInt8(_bytes, _start);
+
+        emit log_named_int('res0', res0);
+
+        assertEq(res0, 0);
+    }
+
+    function test_toInt8_4() public {
+        bytes memory _bytes = hex'ff';
+        uint256 _start = 0;
+
+        int8 res0 = Bytes.toInt8(_bytes, _start);
+
+        emit log_named_int('res0', res0);
+
+        assertEq(res0, -1);
+    }
 }
