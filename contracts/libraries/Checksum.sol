f03176d6f69dbcdbfb6805d5bb5a4a09e9b12e9d
danny7even <danny7even@nugglabs.org>
2021-11-08 02:53:11 +0100

    other shit

--- contracts/libraries/Checksum.sol
+++ contracts/libraries/Checksum.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.4;
  */
 
 library Checksum {
-    function fletcher16(bytes memory data) internal pure returns (bytes2 res) {
+    function fletcher16(bytes memory data) internal pure returns (uint16 res) {
         uint16 sum1 = 0;
         uint16 sum2 = 0;
         for (uint256 index = 0; index < data.length; index++) {
@@ -15,6 +15,6 @@ library Checksum {
             sum2 = (sum2 + sum1) % 255;
         }
 
-        res = bytes2((sum2 << 8) | sum1);
+        res = (sum2 << 8) | sum1;
     }
 }
