d18c1f7babd04dd40b503712bca64b4d0f37ee02
dub6ix <dub6ix@nugglabs.org>
2021-12-04 16:34:11 -0600

    cleanup

--- contracts/src/libraries/BitReader.sol
+++ contracts/src/libraries/BitReader.sol
@@ -18,17 +18,15 @@ library BitReader {
         uint256 pos;
     }
 
-    function init(uint256[] memory input) internal view returns (Memory memory m) {
+    function init(uint256[] memory input) internal pure returns (Memory memory m) {
         m.dat = input;
+
         m.moves = 2;
-        input.length.log('input.length');
 
         m.dat = new uint256[](input[0]);
-        console.log('m.data.length', m.dat.length, input[0]);
+
         for (uint256 i = 1; i < input[0] + 1; i++) {
-            input[i].log('input[i]');
             m.dat[input[0] - i] = input[i];
-            m.dat[input[0] - i].log('m.dat[input[0] - i - 1]');
         }
     }
 
@@ -36,23 +34,17 @@ library BitReader {
         res = m.dat[0] & ShiftLib.mask(bits);
     }
 
-    function select(
-        Memory memory m,
-        uint256 bits,
-        string memory name,
-        uint256 id
-    ) internal view returns (uint256 res) {
-        // m.dat[0].log('m.dat[0]');
+    function select(Memory memory m, uint256 bits) internal pure returns (uint256 res) {
         res = m.dat[0] & ShiftLib.mask(bits);
 
         m.dat[0] = m.dat[0] >> bits;
+
         m.pos += bits;
+
         if (m.pos >= 128) {
             uint256 ptr = (m.moves / 2);
-
             if (ptr < m.dat.length) {
                 m.dat[0] <<= m.pos - 128;
-                // console.log('ptr', ptr);
                 uint256 move = m.dat[ptr] & ShiftLib.mask(128);
                 m.dat[ptr] >>= 128;
                 m.dat[0] |= (move << 128);
@@ -61,8 +53,5 @@ library BitReader {
                 m.pos -= 128;
             }
         }
-        // console.log(res.toHexString(), m.pos, bits, name);
     }
 }
-
-// 01b0fc3f0fc3f0fc3f0fd630fd5440324d0160
--- contracts/src/types/Version.sol
+++ contracts/src/types/Version.sol
@@ -21,13 +21,13 @@ library Version {
             BitReader.Memory memory reader = BitReader.init(data[j]);
 
             // 32 bits: NUGG
-            require(reader.select(32, 'nuggcheck', 0) == 0x4e554747, 'DEC:PI:0');
+            require(reader.select(32) == 0x4e554747, 'DEC:PI:0');
 
-            uint256 feature = reader.select(3, 'feature', 0);
+            uint256 feature = reader.select(3);
 
             uint256[] memory pallet = parsePallet(reader);
 
-            uint256 versionLength = reader.select(2, 'versionLength', 0) + 1;
+            uint256 versionLength = reader.select(2) + 1;
 
             m[j] = new Memory[](versionLength);
 
@@ -41,20 +41,19 @@ library Version {
     }
 
     function parsePallet(BitReader.Memory memory reader) internal view returns (uint256[] memory res) {
-        uint256 palletLength = reader.select(4, 'palletLength', 0) + 1;
+        uint256 palletLength = reader.select(4) + 1;
 
         res = new uint256[]((palletLength) / 7 + 1);
 
         for (uint256 i = 0; i < palletLength; i++) {
             uint256 working = 0;
             // 4 bits: zindex
-            working |= reader.select(4, 'zindex', i) << 32;
+            working |= reader.select(4) << 32;
 
             // 1 or 25 bits: rgb
-            working |= (reader.select(1, 'rgb - init', i) == 0x1 ? 0x000000 : reader.select(24, 'rgb - sec', i)) << 8;
-
+            working |= (reader.select(1) == 0x1 ? 0x000000 : reader.select(24)) << 8;
             // 1 or 8 bits: a
-            working |= reader.select(1, 'a - init', i) == 0x1 ? 0xff : reader.select(8, 'a - sec', i);
+            working |= reader.select(1) == 0x1 ? 0xff : reader.select(8);
 
             res[i / 7] |= working << i % 7;
         }
@@ -62,52 +61,48 @@ library Version {
 
     function parseData(BitReader.Memory memory reader) internal view returns (uint256 res) {
         // 12 bits: coordinate - anchor x and y
-        res |= reader.select(6, 'y height', 0) << 69; // heighth and width
-        res |= reader.select(6, 'x width', 0) << 63;
+        res |= reader.select(6) << 69; // heighth and width
+        res |= reader.select(6) << 63;
         // 12 bits: coordinate - anchor x and y
-        res |= reader.select(6, 'anchor y', 0) << 51;
-        res |= reader.select(6, 'anchor y', 0) << 57;
+        res |= reader.select(6) << 51;
+        res |= reader.select(6) << 57;
 
         // 1 or 25 bits: rlud - radii
-        res |= (reader.select(1, 'radii a', 0) == 0x1 ? 0x000000 : reader.select(24, 'radii b', 0)) << 27;
+        res |= (reader.select(1) == 0x1 ? 0x000000 : reader.select(24)) << 27;
 
         // 1 or 25 bits: rlud - expanders
-        res |= (reader.select(1, 'expander a', 0) == 0x1 ? 0x000000 : reader.select(24, 'expanders b', 0)) << 3;
+        res |= (reader.select(1) == 0x1 ? 0x000000 : reader.select(24)) << 3;
     }
 
     function parseReceivers(BitReader.Memory memory reader) internal view returns (uint256 res) {
-        uint256 receiversLength = reader.select(1, 'receiversLength a', 0) == 0x1 ? 0x1 : reader.select(3, 'receiversLength b', 0);
-
+        uint256 receiversLength = reader.select(1) == 0x1 ? 0x1 : reader.select(3);
         for (uint256 j = 0; j < receiversLength; j++) {
             uint256 receiver = 0;
 
             // yOrYOffset
-            receiver |= (reader.select(6, 'yOrYOffset a', j) << 6) | reader.select(6, 'yOrYOffset b', j);
+            receiver |= reader.select(6) << 6;
 
             //xOrPreset
-            // receiver |= reader.select(6);
+            receiver |= reader.select(6);
 
             // rFeature
-            uint256 rFeature = reader.select(3, 'rFeature a', j) + reader.select(1, 'rFeature b', j) == 0x1 ? 128 : 0;
+            uint256 rFeature = reader.select(3) + reader.select(1) == 0x1 ? 128 : 0;
 
             res |= receiver << (rFeature * 8);
         }
     }
 
     function parseMiniMatrix(BitReader.Memory memory reader, uint256 size) internal view returns (uint256[] memory res) {
-        uint256 groupsLength = reader.select(1, 'groupsLength a', 0) == 0x1
-            ? reader.select(8, 'groupsLength b', 0) + 1
-            : reader.select(16, 'groupsLength c', 0) + 1;
+        uint256 groupsLength = reader.select(1) == 0x1 ? reader.select(8) + 1 : reader.select(16) + 1;
         uint256 height = size & (ShiftLib.mask(6) << 6);
         uint256 width = size >> 6;
         res = new uint256[]((height * width) / 64 + 1);
 
         uint256 index = 0;
         for (uint256 a = 0; a < groupsLength; a++) {
-            uint256 len = reader.select(2, 'len', a) + 1;
-            if (len == 4) len = reader.select(4, '==4', a) + 4;
-            console.log('len', len, groupsLength, a);
-            uint256 key = reader.select(4, 'key      ', a);
+            uint256 len = reader.select(2) + 1;
+            if (len == 4) len = reader.select(4) + 4;
+            uint256 key = reader.select(4);
             for (uint256 i = 0; i < len; i++) res[index / 64] |= key << (4 * (index++ % 64));
         }
 
