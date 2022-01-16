261a4bb67c48e300f713caf33b0e4087e5cbf4ac
dub6ix <dub6ix@nugglabs.org>
2022-01-16 14:30:29 -0600

    all good!

--- src/libraries/MinimalProxy.sol
+++ src/libraries/MinimalProxy.sol
@@ -20,22 +20,6 @@ library MinimalProxy {
     /**
      * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
      *
-     * This function uses the create opcode, which should never revert.
-     */
-    function clone(address implementation) internal returns (address instance) {
-        assembly {
-            let ptr := mload(0x40)
-            mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(ptr, 0x14), shl(0x60, implementation))
-            mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            instance := create(0, ptr, 0x37)
-        }
-        require(instance != address(0), 'ERC1167: create failed');
-    }
-
-    /**
-     * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
-     *
      * This function uses the create2 opcode and a `salt` to deterministically deploy
      * the clone. Using the same `implementation` and `salt` multiple time will revert, since
      * the clones cannot be deployed twice at the same address.
